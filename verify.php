d61f4a3519f4e2e3c4a0d7baeb203acdd090d1be
Kornnalin <32424977+kornnalin@users.noreply.github.com>
2019-03-01 23:49:16 +0700

    Delete verify.php

--- verify.php
+++
@@ -1,9 +0,0 @@
-<?php
-$access_token = '5Drfo4t/S4oZbsMzAbehjM70kuXfWe6Xp6SlLnmVbNwTrYaTJV+D3aQnhsy7CYZ/2lwbs8F90ggBbC4gx4qvAA7eUZ4IuakHjymF+hxQkbLAk9n8/mQfem614F9yf0B0amo64KSPFWTVYZTZ1w5ZfQdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/verify';
-$headers = array('Authorization: Bearer ' .$access_token);
-$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);curl_close($ch);
-echo $result;
-?>
