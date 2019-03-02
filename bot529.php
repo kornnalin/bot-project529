634a537048afe6b74f23c7429a2fd78e7314f25d
firn <firnfroy@gmail.com>
2019-03-02 15:04:41 +0700

    bot529

--- bot529.php
+++ bot529.php
@@ -10,7 +10,7 @@ foreach ($events['events'] as $event) {		// Reply only when message sent is in '
       $text = $event['message']['text'];			// Get replyToken
       $replyToken = $event['replyToken'];			// Build message to reply back
       $messages = ['type' => 'text','text' => $text];	 		// Make a POST Request to Messaging API to reply to sender
-      $sticker = ['type' => 'sticker','packageId' => '3','stickerId' =>'180'];
+      $sticker = ['type' => 'sticker','packageId' => 3,'stickerId' =>180];
       $url = 'https://api.line.me/v2/bot/message/reply';
       $data = ['replyToken' => $replyToken,'messages' => [$messages],[$sticker]];
       $post = json_encode($data);
