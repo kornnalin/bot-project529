16b5c38119a48221d486ad301165eb978b9ceebb
firn <firnfroy@gmail.com>
2019-03-06 20:43:05 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -34,8 +34,8 @@ if (sizeof($request_array['events']) > 0) {
         $reply_token = $event['replyToken']; // Build message to reply back
         $data = ['replyToken' => $reply_token,
                  'messages' => [
-                    ['type' => 'text','text' => $userID,]
-                  ]
+                    ['type' => 'text','text' => $userID,];
+                  ],
                 ];
         $post_body = json_encode($data);
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
