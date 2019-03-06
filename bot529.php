651964b24295fec565e776e169a2c40c7387ecf4
firn <firnfroy@gmail.com>
2019-03-06 15:28:35 +0700

    no message

--- bot529.php
+++ bot529.php
@@ -2,7 +2,7 @@
 $API_URL = 'https://api.line.me/v2/bot/message'; // URL API LINE
 $ACCESS_TOKEN = '5Drfo4t/S4oZbsMzAbehjM70kuXfWe6Xp6SlLnmVbNwTrYaTJV+D3aQnhsy7CYZ/2lwbs8F90ggBbC4gx4qvAA7eUZ4IuakHjymF+hxQkbLAk9n8/mQfem614F9yf0B0amo64KSPFWTVYZTZ1w5ZfQdB04t89/1O/w1cDnyilFU=';
 $CHANNEL_SECRET = '96164a13e36916b36e7769c5c49b6b40';
-
+$userID = '';
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN); // Set HEADER
 $request = file_get_contents('php://input'); // Get request content
 $request_array = json_decode($request, true); // Decode JSON to Array
@@ -33,7 +33,6 @@ if ( sizeof($request_array['events']) > 0 ) {
       $reply_message =  $event['message']['text'];
       $userID = $event['message']['source'];
       // echo $reply_message."<br>";
-      echo $userID."<br>";
       $reply_token = $event['replyToken']; // Build message to reply back
       $data = [
          'replyToken' => $reply_token,
@@ -50,5 +49,5 @@ if ( sizeof($request_array['events']) > 0 ) {
    }
 }
 echo "Bot 529 OK";
-
+echo $userID."<br>";
 ?>
