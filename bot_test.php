49444817bfb522d6d4d96c13dd2be14ad7db58cf
firn <firnfroy@gmail.com>
2019-03-07 01:27:46 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -25,6 +25,21 @@ function flexMeassge(){
 
 }
 
+function TagImage($text){
+  $reply_token = $event['replyToken']; // Build message to reply back
+  $data = ['replyToken' => $reply_token,
+           'messages' => [
+             ['type' => 'text','text' => $json_encode],
+             ['type' => 'text','text' => 'GroupID : '.$groupID],
+             ['type' => 'text','text'=> 'UserID : '.$userID],
+             ['type' => 'text','text'=>$text],
+             ['type' => 'text','text' => $tag],
+            ]
+          ];
+  $post_body = json_encode($data);
+  $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+}
+
 //เป็นการ Get ข้อมูลที่ได้จากการที่ User ที่มีการกระทำใน Channel
 if (sizeof($request_array['events']) > 0) {
       // $json_encode = json_encode($request_array);
@@ -34,20 +49,9 @@ if (sizeof($request_array['events']) > 0) {
         $groupID = $event['source']['groupId'];
         $text = $event['message']['text'];
         if($text == 'T' || $text == 'Tag' || $text == 'แท็ก'){
-          $tag = 'TAG';
+          TagImage($text);
         }
-        $reply_token = $event['replyToken']; // Build message to reply back
-        $data = ['replyToken' => $reply_token,
-                 'messages' => [
-                   ['type' => 'text','text' => $json_encode],
-                   ['type' => 'text','text' => 'GroupID : '.$groupID],
-                   ['type' => 'text','text'=> 'UserID : '.$userID],
-                   ['type' => 'text','text'=>$text],
-                   ['type' => 'text','text' => $tag], 
-                  ]
-                ];
-        $post_body = json_encode($data);
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
    }
 }
 echo "Bot 529 OK";
