3db885df2668c8670dba66a351bb747be3ea02ac
firn <firnfroy@gmail.com>
2019-03-07 16:21:08 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -5,7 +5,7 @@ $CHANNEL_SECRET = '96164a13e36916b36e7769c5c49b6b40';
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN); // Set HEADER
 $request = file_get_contents('php://input'); // Get request content
 $request_array = json_decode($request, true); // Decode JSON to Array
-$keyword_tag = {'T','t','TAG','Tag','แท็ก','แท๊ก'};
+$keyword_tag = array('T','t','TAG','Tag','แท็ก','แท๊ก');
 echo $keyword_tag."<br>";
 //สร้าง Function สำหรับ CURL ใช้ในการ Post Data ไปยัง API ของ Line
 function send_reply_message($url, $post_header, $post_body)
