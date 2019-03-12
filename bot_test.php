3cbbd4fa4c0f4c6b1c136c8dccd6fe0d2a6b42b3
firn <firnfroy@gmail.com>
2019-03-12 13:48:53 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -70,7 +70,7 @@ if (sizeof($request_array["events"]) > 0) {
           $post_body = json_encode($data);
           $send_result = send_reply_message($API_URL."/reply", $POST_HEADER, $post_body);
         }
-        if($text == "flex"){
+        /*if($text == "flex"){
           $reply_token = $event["replyToken"]; // Build message to reply back
           $data = ["replyToken" => $reply_token,
           "messages" => [
@@ -110,7 +110,7 @@ if (sizeof($request_array["events"]) > 0) {
                  ];
                  $post_body = json_encode($data);
                  $send_result = send_reply_message($API_URL."/reply", $POST_HEADER, $post_body);
-        }
+        }*/
 }
 }
 echo "Bot 529 OK";
