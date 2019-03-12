ac6f2da90482995d2253ec55ba92da3ea0481228
firn <firnfroy@gmail.com>
2019-03-12 14:02:42 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -70,11 +70,10 @@ if (sizeof($request_array["events"]) > 0) {
           $post_body = json_encode($data);
           $send_result = send_reply_message($API_URL."/reply", $POST_HEADER, $post_body);
         }
-        if($text == "flex"){
+        if($text == "a"){
           $reply_token = $event["replyToken"]; // Build message to reply back
           $data = ["replyToken" => $reply_token,
           "messages" => [
-
                       "type"=> "template",
                       "altText"=> "this is a carousel template",
                       "template"=> [
