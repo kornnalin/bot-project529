d44589d9a579833b0bc82476777ff3204fa3db31
firn <firnfroy@gmail.com>
2019-03-14 18:17:59 +0700

    no message

--- bot_testy.php
+++ bot_testy.php
@@ -21,9 +21,74 @@ function send_reply_message($url, $post_header, $post_body)
     return $result;
 }
 
-function flexMeassge(){
-
-}
+function flexMeassge_Tag(){
+  $flexMessageTag = array(
+    "type"=>"flex",
+    "altText"=>"Flex Message",
+    "contents" =>
+      [
+        array(
+          "type" => "bubble",
+          "direction"=>"ltr",
+          "header" => array(
+            "type"=>"box",
+            "layout"=>"vertical",
+            "contents"=>
+              [
+                array(
+                   "type"=> "text",
+                   "text"=> "ใครมาทำงานแล้วบ้างน้า",
+                   "size"=> "lg",
+                   "align"=> "center",
+                   "weight"=> "bold",
+                   "color"=> "#000000"
+                ),
+                array(
+                   "type"=> "text",
+                   "text"=> "มาดูกันเถอะ!!",
+                   "margin"=> "none",
+                   "size"=> "md",
+                   "align"=> "start",
+                   "color"=> "#A81F1F"
+                )
+              ]
+          ),
+          "footer"=> array(
+            "type"=>"box",
+            "layout"=>"horizontal",
+            "contents"=>
+              [
+                array(
+                    "type"=> "button",
+                    "action"=> array(
+                      "type"=> "uri",
+                      "label"=> "แท็กภาพถ่าย",
+                      "uri"=>"line://app/1609271731-Ony6BL0g"
+                    )
+                ),
+                array(
+                  "type"=> "button",
+                   "action"= array(
+                     "type"=> "uri",
+                     "label"=> "สถานะการแท็ก",
+                     "uri"=> "line://app/1609271731-YqDJROo0"
+                   )
+                )
+              ]
+          ),
+          "style"=>array(
+            "header"=> array(
+              "backgroundColor"=>"F7DB00"
+            ),
+            "footer"=>array(
+               "backgroundColor"=> "#FBF4C0"
+            )
+          )
+        )
+      ]
+  );
+  return $flexMessageTag;
+};
 
 function getBubble( $title, $img_url, $btn_url ) {
 	$bubble = array(
@@ -81,15 +146,20 @@ if (sizeof($request_array['events']) > 0) {
           if($text == $tag){
             $tag = 'TAG';
             $reply_token = $event['replyToken']; // Build message to reply back
-            $data = ['replyToken' => $reply_token,
-                     'messages' => [
-                       ['type' => 'text','text' => $json_encode],
-                       ['type' => 'text','text' => 'GroupID : '.$groupID],
-                       ['type' => 'text','text'=> 'UserID : '.$userID],
-                       ['type' => 'text','text'=>$text],
-                       ['type' => 'text','text' => $tag],
-                      ]
-                    ];
+            $message = flexMeassge_Tag();
+            $data = [
+              'replyToken' => $reply_token,
+              'messages' => [$messages],
+            ]
+            // $data = ['replyToken' => $reply_token,
+            //          'messages' => [
+            //            ['type' => 'text','text' => $json_encode],
+            //            ['type' => 'text','text' => 'GroupID : '.$groupID],
+            //            ['type' => 'text','text'=> 'UserID : '.$userID],
+            //            ['type' => 'text','text'=>$text],
+            //            ['type' => 'text','text' => $tag],
+            //           ]
+            //         ];
             $post_body = json_encode($data);
             $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
           }
