92a2cb87b45248aeaf762169f9395623778c9f2e
firn <firnfroy@gmail.com>
2019-03-16 00:12:46 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -1,116 +1,45 @@
-<?php
-$API_URL = "https://api.line.me/v2/bot/message"; // URL API LINE
-$ACCESS_TOKEN = "5Drfo4t/S4oZbsMzAbehjM70kuXfWe6Xp6SlLnmVbNwTrYaTJV+D3aQnhsy7CYZ/2lwbs8F90ggBbC4gx4qvAA7eUZ4IuakHjymF+hxQkbLAk9n8/mQfem614F9yf0B0amo64KSPFWTVYZTZ1w5ZfQdB04t89/1O/w1cDnyilFU=";
-$CHANNEL_SECRET = "96164a13e36916b36e7769c5c49b6b40";
-$POST_HEADER = array("Content-Type: application/json", "Authorization: Bearer " . $ACCESS_TOKEN); // Set HEADER
-$request = file_get_contents("php://input"); // Get request content
-$request_array = json_decode($request, true); // Decode JSON to Array
-$keyword_tag = array("T","t","TAG","Tag","แท็ก","แท๊ก","แท็กภาพถ่าย","สถานะการแท็ก","Status Tag","status tag","Status tag","status Tag");
-$keyword_report = array("R","r","Report","Reports","report","reports","รายงาน","เช็คเวลาเข้าออกงาน","รายงานเวลาเข้าออกงาน");
-//สร้าง Function สำหรับ CURL ใช้ในการ Post Data ไปยัง API ของ Line
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-}
+<?php // callback.php
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+$access_token = 'XXXXXXXXX';
 
-function flexMeassge(){
-
-
-}
-
-function setPostMeassge(){
-
-}
-
-//เป็นการ Get ข้อมูลที่ได้จากการที่ User ที่มีการกระทำใน Channel
-if (sizeof($request_array["events"]) > 0) {
-      // $json_encode = json_encode($request_array);
-      foreach ($request_array["events"] as $event) {
-        $json_encode = json_encode($request_array);
-        $userID = $event["source"]["userId"];
-        $groupID = $event["source"]["groupId"];
-        $text = $event["message"]["text"];
-
-        foreach ($keyword_tag as $key => $tag) {
-          if($text == $tag){
-            $tag = "TAG";
-            $reply_token = $event["replyToken"]; // Build message to reply back
-            $data = ["replyToken" => $reply_token,
-                     "messages" => [
-                       ["type" => "text","text" => $json_encode],
-                       ["type" => "text","text" => "GroupID : ".$groupID],
-                       ["type" => "text","text"=> "UserID : ".$userID],
-                       ["type" => "text","text"=>$text],
-                       ["type" => "text","text" => $tag],
-                      ]
-                    ];
-            $post_body = json_encode($data);
-            $send_result = send_reply_message($API_URL."/reply", $POST_HEADER, $post_body);
-          }
-        }
-
-        if($text == "hi"|| $text=="hello"){
-          $reply_token = $event["replyToken"]; // Build message to reply back
-          $data = ["replyToken" => $reply_token,
-                   "messages" => [
-                     ["type" => "text","text" => $json_encode],
-                     ["type" => "text","text" => "GroupID : ".$groupID],
-                     ["type" => "text","text"=> "UserID : ".$userID],
-                     ["type" => "text","text"=>$text],
-                    ]
-                  ];
-          $post_body = json_encode($data);
-          $send_result = send_reply_message($API_URL."/reply", $POST_HEADER, $post_body);
-        }
-        if($text == "a"){
-          $reply_token = $event["replyToken"]; // Build message to reply back
-          $data = ["replyToken" => $reply_token,
-          "messages" => [
-                      "type"=> "flex",
-                      "altText"=> "this is a carousel template",
-                      "contents"=> [
-                      "type"=> "carousel",
-                      "actions"=> [],
-                      "columns"=> [
-                      [
-                        "thumbnailImageUrl"=> "https://cp.su.ac.th/image/crop/327",
-                        "title"=> "ภาพนี้เป็นใครน้า",
-                        "text"=> " มาช่วยกันแท็ก หาเจ้าของภาพกันเถอะ",
-                        "actions"=> [
-                          [
-                            "type"=> "message",
-                            "label"=> "คุณหมาย",
-                            "text"=> "ทำการแท็ก คุณหมาย"
-                          ],
-                          [
-                            "type"=> "message",
-                            "label"=> "คุณแอน",
-                            "text"=> "ทำการแท็ก คุณแอน"
-                          ],
-                          [
-                            "type"=> "message",
-                            "label"=> "คุณประวิม",
-                            "text"=> "ทำการแท็ก คุณประวิม"
-                          ]
-                        ]
-                      ]
-                      ]
-
-                  ]
-                ],
-                 ];
-                 $post_body = json_encode($data);
-                 $send_result = send_reply_message($API_URL."/reply", $POST_HEADER, $post_body);
-        }
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+// Loop through each event
+foreach ($events['events'] as $event) {
+// Reply only when message sent is in 'text' format
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+// Get text sent
+$text = $event['source']['userId'];
+// Get replyToken
+$replyToken = $event['replyToken'];
+// Build message to reply back
+$messages = [
+'type' => 'text',
+'text' => $text
+];
+// Make a POST Request to Messaging API to reply to sender
+$url = 'https://api.line.me/v2/bot/message/reply';
+$data = [
+'replyToken' => $replyToken,
+'messages' => [$messages],
+];
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
 }
 }
-echo "Bot 529 OK";
-?>
+}
+echo "OK";
