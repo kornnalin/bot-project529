dc59f9ec910b5cddd167c4e39b432c45ebd31219
firn <firnfroy@gmail.com>
2019-03-16 00:54:40 +0700

    no message

--- bot_test.php
+++ bot_test.php
@@ -5,12 +5,13 @@ $content = file_get_contents('php://input');// Parse JSON
 $events = json_decode($content, true);// Validate parsed JSON data
 if(!is_null($events['events'])) {	// Loop through each event
 foreach ($events['events'] as $event) {		// Reply only when message sent is in 'text' format
+  $json_encode = json_encode($event);
   if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent
       $text = $event['message']['text'];			// Get replyToken
       $replyToken = $event['replyToken'];			// Build message to reply back
       $messages = ['type' => 'text','text' => $text];			// Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/reply';
-      $data = ['replyToken' => $replyToken,'messages' => [$messages],];
+      $data = ['replyToken' => $replyToken,'messages' => [$messages],[$json_encode],];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
       $ch = curl_init($url);
